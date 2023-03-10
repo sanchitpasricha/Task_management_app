d18d57deaf74cc975a3601e249988c2dfebfb52f
sanchitpasricha <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>
2023-03-10 13:12:32 +0530

    creating stateful checkbox

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -3,12 +3,31 @@ import 'package:flutter/material.dart';
 class TaskTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const ListTile(
+    return ListTile(
       title: Text('A task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: TextCheckBox(),
+    );
+  }
+}
+
+class TextCheckBox extends StatefulWidget {
+  @override
+  State<TextCheckBox> createState() => _TextCheckBoxState();
+}
+
+class _TextCheckBoxState extends State<TextCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (newValue) {
+        setState(() {
+          isChecked = newValue!;
+        });
+      },
     );
   }
 }
